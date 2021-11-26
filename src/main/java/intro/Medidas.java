f7ac2b2658306f1ed6651dcd5da44cbad89b23b6
Home x <gustavobarros12654@gmail.com>
2021-11-26 04:11:02 -0200

    Incluido o check list

---
+++ src/main/java/intro/Medidas.java
@@ -0,0 +1,22 @@
+package intro;
+
+
+public class Medidas {
+
+    public static void main(String[] args){
+
+
+        int largura;
+        int comprimento;
+        int resultado;
+
+
+        largura=4;
+        comprimento=3;
+
+        resultado=largura*comprimento;
+        System.out.println("A área é de " + resultado + " m2");
+    }
+
+
+}
